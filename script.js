3af648ec5a089de0c45412e3c5a7013d7d61bf75
astainy1 <astainyharris1@gmail.com>
2024-07-04 15:43:13 +0000

    Updated all files

--- script.js
+++ script.js
@@ -20,31 +20,36 @@ let typeWPM = 0;
 let typeTimeLeft = 0;
 let typeMistake = 0;
 
-//get data from JSON file
-fetch("data.json")
-.then((response) => {
+//asynchronize function to get data from JSON file
+async function fetchData(){
+    fetch("data.json")
+    .then((response) => {
 
-    if(!response.ok){
-        throw new Error(`HTTP Response! Status: ${response.status}`)
-    }
+        if(!response.ok){
+            throw new Error(`HTTP Response! Status: ${response.status}`)
+        }
 
-    return response.json();
-} 
-)
-.then((data) => {
+        return response.json();
+    } 
+    )
+    .then((data) => {
 
-    let arrayElement = data;
-    let randomJsonData = Math.floor(Math.random() * arrayElement.length);
-    let displayElement = data[randomJsonData];
+        let arrayElement = data;
+        let randomJsonData = Math.floor(Math.random() * arrayElement.length);
+        let displayElement = data[randomJsonData];
 
-    paragraph.textContent = displayElement;
-    console.log(displayElement)
+        paragraph.textContent = displayElement;
+        console.log(displayElement)
 
-})
+    })
 
-.catch((error) =>{
-    console.log("Trouble fetching data from Json file!", error);
-})
+    .catch((error) =>{
+        console.log("Trouble fetching data from Json file!", error);
+    })
+}
+
+fetchData();
+
 
 
 
